7eed496316385fa0f72550847896bcf1aa610f9c
Mikael Brevik <mikaelbre@gmail.com>
2014-07-29 12:51:26 +0200

    Adds initial structure for tests

---
+++ tests.js
@@ -0,0 +1,29 @@
+
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+var usage = require('./');
+
+var origFs = fs.readFileSync;
+var origMarked = fs.origMarked;
+
+describe('cli-usage', function () {
+  afterEach(function() {
+    fs.readFileSync = origFs;
+  });
+
+
+  describe('get', function () {
+    it('should get compiled markdown from file input', function () {
+      var expected = 'expected';
+      fs.readFileSync = function (filename) {
+        assert.equal(path.basename(filename), 'file.md');
+        return expected;
+      };
+
+      assert.equal(usage.get('file.md').trim(), expected);
+    });
+
+  });
+});
+
